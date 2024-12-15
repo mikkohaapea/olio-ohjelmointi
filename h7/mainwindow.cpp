15269f0937f0673427c1456fc6f02cd017bb4b45
Mikko Haapea <mikkohaapea@hotmail.com>
2024-12-15 22:25:47 +0200

    lisätty metodi updateDisplay, johon kerätty toistuva koodi buttonien sloteista (counterin muuttaminen QStringiksi ja tulostetun tekstin päivitys). Tätä metodia kutsutaan em. sloteissa

--- h7/mainwindow.cpp
+++ h7/mainwindow.cpp
@@ -13,13 +13,25 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::updateDisplay(int)
+{
+    // Muuttaa counter-muuttujan arvon QStringiksi ja päivittää
+    // txtResult -widgetissä ja labelInfo -widgetissä näkyvän luvun
+    QString s = QString::number(counter);
+    ui->txtResult->setText(s);
+    ui->labelInfo->setText("Painiketta painettu " + s + " kertaa");
+}
+
 void MainWindow::on_btnCount_clicked()
 {
     counter++;
     qDebug()<<counter;
+    updateDisplay(counter);
+    /*
     QString s = QString::number(counter);
     ui->txtResult->setText(s);
     ui->labelInfo->setText("Painiketta painettu "+s+" kertaa");
+    */
 }
 
 
@@ -27,8 +39,11 @@ void MainWindow::on_btnReset_clicked()
 {
     counter = 0;
     qDebug()<<counter;
+    updateDisplay(counter);
+    /*
     QString s = QString::number(counter);
     ui->txtResult->setText(s);
     ui->labelInfo->setText("Painiketta painettu " + s + " kertaa");
+    */
 }
 
